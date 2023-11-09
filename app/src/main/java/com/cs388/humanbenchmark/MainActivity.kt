0470eb9bee5e59881d416e6535e83a3537ea4063
breakthegreat <bloodmodii@gmail.com>
2023-11-08 22:48:27 -0500

    added file for debug

--- app/src/main/java/com/cs388/humanbenchmark/MainActivity.kt
+++ app/src/main/java/com/cs388/humanbenchmark/MainActivity.kt
@@ -2,7 +2,6 @@ package com.cs388.humanbenchmark
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
@@ -14,19 +13,21 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.Task
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth : FirebaseAuth
-    private lateinit var googleSignInClient : GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
     lateinit var drawerLayout: DrawerLayout
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
@@ -37,78 +38,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        auth = FirebaseAuth.getInstance()
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
 
 
 
+       // TODO : initiliaze the fragment, create logic behind it.
 
-     findViewById<Button>(R.id.signInBtn).setOnClickListener { // sign in button
-           signInGoogle()
-       }
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+
+        val homeFragment: Fragment = homeFragment()
+        val gameFragment: Fragment = gameFragment()
+        val leaderboardFragment: Fragment = leaderboardFragment()
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.action_home -> fragment = homeFragment
+                R.id.action_games -> fragment = gameFragment
+               R.id.action_leaderboard -> fragment = leaderboardFragment
+            }
+        replaceFragment(fragment)
             true
-        } else super.onOptionsItemSelected(item)
-    }
-
-    private fun signInGoogle(){
-        val signInIntent = googleSignInClient.signInIntent
-        Log.d("TEST","SUCCESS-1")
-        launcher.launch(signInIntent)
-    }
-
-    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-        result ->
-                if (result.resultCode == Activity.RESULT_OK){
-                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                    //this isn't being reached
-                    Log.d("TEST","SUCCESS0")
-                    handleResults(task)
-                }
-                else{
-                    Log.d("TEST", result.resultCode.toString())
-                }
-    }
-
-    private fun handleResults(task: Task<GoogleSignInAccount>)  {
-        if (task.isSuccessful){
-            val account : GoogleSignInAccount? = task.result
-            if (account != null){
-                Log.d("TEST","SUCCESS1")
-                updateUI(account)
-            }
         }
-        else{
-            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
-        }
+
+
+
+
     }
-    @SuppressLint("SuspiciousIndentation")
-    private fun updateUI(account: GoogleSignInAccount){
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        auth.signInWithCredential(credential).addOnCompleteListener{
-            if (it.isSuccessful){
 
+    private fun replaceFragment(fragment: Fragment) {
 
-            var username = findViewById<TextView>(R.id.username)
-                username.text = "Welcome,\n" + account.displayName
-            var image = findViewById<ImageView>(R.id.profilePicture)
-                Glide.with(this).load(account.photoUrl).into(image)
-
-
-
-            }
-            else{
-                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-            }
-        }
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_frame_layout, fragment)
+        fragmentTransaction.commit()
     }
+
+
+
+
 }
